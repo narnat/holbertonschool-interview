4cc123f808853b42abb3a736bdd59bf50f62b601
Farrukh <farruh1996@gmail.com>
2020-02-04 13:18:33 -0800

    Add lockboxes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -8,9 +8,18 @@ def canUnlockAll(boxes):
     visited = {0}
     stack_of_keys = [0]
 
+    # if isinstance(boxes, list):
+    #     for b in boxes:
+    #         if not isinstance(b, list):
+    #             return False
+    #         for ki in range(len(b)):
+    #             if not isinstance(b[ki], int):
+    #                 return False
+    #             if b[ki] < 0 or b[ki] > len(boxes) - 1:
+    #                 del b[ki]
     while stack_of_keys:
         room = stack_of_keys.pop()
-        if room >= len(boxes):
+        if room >= len(boxes) or room < 0:
             continue
         for key in boxes[room]:
 
