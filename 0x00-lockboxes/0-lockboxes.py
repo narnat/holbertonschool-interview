01886ce7a4d5de0bb0a888837459bae2c190c1c8
Farrukh <farruh1996@gmail.com>
2020-02-04 13:26:32 -0800

    Remove if not list check

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -3,20 +3,11 @@
 
 def canUnlockAll(boxes):
 
-    if not boxes or len(boxes) == 0:
+    if len(boxes) == 0:
         return True
     visited = {0}
     stack_of_keys = [0]
 
-    # if isinstance(boxes, list):
-    #     for b in boxes:
-    #         if not isinstance(b, list):
-    #             return False
-    #         for ki in range(len(b)):
-    #             if not isinstance(b[ki], int):
-    #                 return False
-    #             if b[ki] < 0 or b[ki] > len(boxes) - 1:
-    #                 del b[ki]
     while stack_of_keys:
         room = stack_of_keys.pop()
         for key in boxes[room]:
