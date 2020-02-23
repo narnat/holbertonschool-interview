eef5a46d85c09f643491f083b7211b384c105fbc
Farrukh <farruh1996@gmail.com>
2020-02-23 12:49:01 -0800

    Add condition if n is less than 1

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -7,18 +7,20 @@ execute only two operations in this file: Copy All and Paste. Given a
 number n, write a method that calculates the fewest number of operations
 needed to result in exactly n H characters in the file.
 
-    Solution:
+    Solution: the problem boils down to find the prime factors and return their
+sum.
 """
 
 
 def sum_of_prime_factors(n):
     res = 0
+
+    if n <= 1:
+        return res
     for i in range(2, int(math.sqrt(n) + 1)):
         while n % i == 0:
             n = n // i
             res += i
-            if n < 2:
-                break
     if n >= 2:  # Case where n is prime number
         res += n
     return res
