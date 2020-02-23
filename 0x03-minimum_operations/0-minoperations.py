c10d279f412b2044356500cff7fb705a1bb25389
Farrukh <farruh1996@gmail.com>
2020-02-23 12:43:21 -0800

    Add effiecient solution

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import math
 """"
 
     In a text file, there is a single character H. Your text editor can
@@ -6,25 +7,20 @@ execute only two operations in this file: Copy All and Paste. Given a
 number n, write a method that calculates the fewest number of operations
 needed to result in exactly n H characters in the file.
 
+    Solution:
 """
 
 
+def sum_of_prime_factors(n):
+    res = 0
+    for i in range(2, int(math.sqrt(n) + 1)):
+        while n % i == 0:
+            n = n // i
+            res += i
+    if n >= 2:  # Case where n is prime number
+        res += n
+    return res
+
+
 def minOperations(n):
-    if (n <= 1):
-        return 0
-
-    result = float('inf')
-
-    def minOps(l, ops, p):
-        if l == n:
-            nonlocal result
-            result = min(result, ops)
-            return
-        if l > n:
-            return
-
-        minOps(l * 2, ops + 2, l)
-        minOps(l + p, ops + 1, p)
-
-    minOps(2, 2, 1)
-    return result
+    return sum_of_prime_factors(n)
