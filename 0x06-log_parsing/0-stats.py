fcea718c79e4e2c751ecd61a397f86499fa24bf6
Farrukh <farruh1996@gmail.com>
2020-03-14 23:53:39 -0700

    Add logparser

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+import sys
+"""Log Parser"""
+
+if __name__ == '__main__':
+
+    def printer(size, d):
+        """Printer function"""
+        a = sorted(d.keys())
+        print("File size: {:d}".format(size))
+        for i in a:
+            if d[i] != 0:
+                print("{}: {}".format(i, d[i]))
+
+    size = 0
+    d = {"200": 0, "301": 0, "400": 0, "401": 0,
+         "403": 0, "404": 0, "405": 0, "500": 0}
+
+    counter = 0
+    try:
+        for line in sys.stdin:
+            counter += 1
+            arr = line.split()
+            try:
+                size += int(arr[-1])
+            except:
+                pass
+            try:
+                st = arr[-2]
+                if st in d:
+                    d[st] += 1
+            except:
+                pass
+            if counter % 10 == 0:
+                printer(size, d)
+            printer(size, d)
+    except KeyboardInterrupt:
+        printer(size, d)
+        raise
