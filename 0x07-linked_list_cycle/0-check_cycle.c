d98ebd480af5ea2c36c9ea6c0a371a5a20c2d90e
Farrukh <farruh1996@gmail.com>
2020-03-25 12:45:49 -0700

    Add files for linked list cycle

---
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks cycle in singly linked list
+ * @list: head of linked list
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow, *fast;
+
+	if (!list)
+		return (0);
+
+	slow = list;
+	fast = list->next->next;
+
+	while (fast && fast->next)
+	{
+		if (slow->n == fast->n)
+			return (1);
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	return (0);
+}
