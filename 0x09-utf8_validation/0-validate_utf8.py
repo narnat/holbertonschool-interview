6e2f989787f49e846412ada82b6ecec9ae32cb8f
Farrukh <farruh1996@gmail.com>
2020-04-07 12:30:49 -0700

    Change ifelse order

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -12,14 +12,14 @@ def validUTF8(data):
             byte_cnt -= 1
             if (((n & 0xff) >> 6) != 2):
                 return False
-        elif not (n & 0x80):      # 1 byte
-            byte_cnt = 0
         elif (n & 0xF0) == 0xF0:  # 4 byte
             byte_cnt = 3
         elif (n & 0xE0) == 0xE0:  # 3 byte
             byte_cnt = 2
         elif (n & 0xC0) == 0xC0:  # 2 byte
             byte_cnt = 1
+        elif not (n & 0x80):      # 1 byte
+            byte_cnt = 0
         else:
             return False
     return not byte_cnt
