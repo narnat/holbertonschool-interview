db47376e8fde058b7ec91f94d25b976f8a4682fd
Farrukh <farruh1996@gmail.com>
2020-04-07 12:29:07 -0700

    Change ifelf order

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -10,16 +10,16 @@ def validUTF8(data):
     for n in data:
         if byte_cnt:
             byte_cnt -= 1
-            if ((n >> 6) != 2):
+            if (((n & 0xff) >> 6) != 2):
                 return False
         elif not (n & 0x80):      # 1 byte
             byte_cnt = 0
-        elif (n & 0xC0) == 0xC0:  # 2 byte
-            byte_cnt = 1
-        elif (n & 0xE0) == 0xE0:  # 3 byte
-            byte_cnt = 2
         elif (n & 0xF0) == 0xF0:  # 4 byte
             byte_cnt = 3
+        elif (n & 0xE0) == 0xE0:  # 3 byte
+            byte_cnt = 2
+        elif (n & 0xC0) == 0xC0:  # 2 byte
+            byte_cnt = 1
         else:
             return False
     return not byte_cnt
