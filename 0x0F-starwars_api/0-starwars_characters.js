bd4cd504b8eb3fc8c84cdd76db121ea30e880366
Farrukh <farruh1996@gmail.com>
2020-05-31 22:48:37 -0700

    Add task 0

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+const request = require('request');
+
+async function send (person) {
+  return new Promise(function (resolve, reject) {
+    request(person, function (error, response, body) {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(JSON.parse(body).name);
+      }
+    });
+  });
+}
+
+request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
+  async function (error, response, body) {
+    if (error) {
+      console.log(error);
+    } else {
+      const people = JSON.parse(body).characters;
+      for (let i = 0; i < people.length; ++i) {
+        const name = await send(people[i]);
+        console.log(name, i);
+      }
+    }
+  });
