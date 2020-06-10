25be0e482a3dc032afd846e48e02f66067ec13f1
Farrukh <farruh1996@gmail.com>
2020-06-09 17:57:30 -0700

    Add rain water problem

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+''' Trapping rain water problem '''
+
+
+def rain(walls):
+    '''
+    Function to calculate how much water can be trapped
+    @walls: Walls
+    Return: Amount of water which can be trapped
+    '''
+    left, right = 0, len(walls) - 1
+    water = 0
+    level = 0
+    while left < right:
+        if walls[left] < walls[right]:
+            cur_lvl = walls[left]
+            left += 1
+        else:
+            cur_lvl = walls[right]
+            right -= 1
+        level = max(level, cur_lvl)
+        water += level - cur_lvl
+    return water
