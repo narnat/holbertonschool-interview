6dd1e3e3602114a52d2008b8dad318fcce815f75
Farrukh Akhrarov <farruh1996@gmail.com>
2020-10-14 11:49:53 -0700

    Add regex_wildcmp

---
+++ 0x20-regex/regex.c
@@ -0,0 +1,28 @@
+#include "regex.h"
+
+/**
+* regex_match - matches patterns ('*' and '.') with string
+* @str: string
+* @pattern: pattern which contains alphabetic chars, '*', and '.'
+* Return: 1 if string matches pattern 0 otherwise
+*/
+int regex_match(char const *str, char const *pattern)
+{
+	if (!*str)
+	{
+		if (*(pattern + 1) == '*')
+			return (regex_match(str, pattern + 2));
+		return (*pattern == '\0');
+	}
+	if (*(pattern + 1) == '*')
+	{
+		if (*pattern == '.')
+			return (regex_match(str + 1, pattern) || regex_match(str, pattern + 2));
+		return (regex_match(str + 1, pattern) || regex_match(str, pattern + 2));
+	}
+	if (*pattern == '.' || *pattern == *str)
+	{
+		return (regex_match(str + 1, pattern + 1));
+	}
+	return (0);
+}
